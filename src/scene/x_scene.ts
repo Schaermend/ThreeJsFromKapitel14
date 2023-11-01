71eb3963c546bdd483310f0a579fbdc677b74441
Manfred Schneider <manfred.schneider@dg-email.de>
2023-11-01 07:57:30 +0100

    Zwei Spheren die sich umkreisen

--- src/scene/x_scene.ts
+++ src/scene/x_scene.ts
@@ -1,4 +1,4 @@
-import { SphereGeometry, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer, Mesh } from "three";
+import { SphereGeometry, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer, Mesh, Object3D } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export class XScene{
@@ -7,9 +7,12 @@ export class XScene{
     private camera: PerspectiveCamera
     private renderer=new WebGLRenderer;
     private readonly scene:Scene;
-    private geometry: SphereGeometry;
+    private geometry1: SphereGeometry;
+    private geometry2: SphereGeometry;
     private material: MeshBasicMaterial;
     private sphere: Mesh
+    private child: Mesh
+    private object: Object3D
 
     private constructor(){
         this.camera=new PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1, 1000);
@@ -20,10 +23,16 @@ export class XScene{
         document.body.appendChild(this.renderer.domElement);
         window.addEventListener('resize', this.onWindowResize, false);
         new OrbitControls(this.camera, this.renderer.domElement);
-        this.geometry=new SphereGeometry(16,30,30);
+        this.geometry1=new SphereGeometry(16,30,30);
+        this.geometry2=new SphereGeometry(8,30,30);
         this.material=new MeshBasicMaterial({color: 0xff0000, wireframe: true})
-        this.sphere=new Mesh(this.geometry, this.material);
+        this.object=new Object3D();
+        this.child=new Mesh(this.geometry2, this.material);
+        this.child.position.set(20,20,20);
+        this.object.add(this.child);
+        this.sphere=new Mesh(this.geometry1, this.material);
         this.scene.add(this.sphere);
+        this.scene.add(this.object);
 
     }
     public static getInstance(){
@@ -38,6 +47,8 @@ export class XScene{
     animate=()=>{
         requestAnimationFrame(this.animate)
         this.sphere.rotation.x+=0.01;
+        this.child.rotation.y+=0.01;
+        this.object.rotation.y-=-0.01;
         this.render();
     }
     render(){
