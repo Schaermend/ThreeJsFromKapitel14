2d3d3196243a1fcfb0c39814f6fa72f3e4635b17
Schaermend <manfred.schneider@dg-email.de>
2024-09-08 13:47:36 +0200

    Kapitel 14

--- src/scene/x_scene.ts
+++ src/scene/x_scene.ts
@@ -1,58 +1,69 @@
-import { MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer, Mesh, BoxGeometry, Vector3, AxesHelper } from "three";
+import { PerspectiveCamera, Scene, WebGLRenderer, Mesh, AxesHelper, PointLight, MeshPhongMaterial, SphereGeometry, Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
-import Stats  from 'three/examples/jsm/libs/stats.module.js';
-import { GUI } from 'dat.gui';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { GUI } from "dat.gui";
 
 
 export class XScene{
 
     private static instance=new XScene();
-    private camera: PerspectiveCamera
+    private camera: PerspectiveCamera;
     private renderer=new WebGLRenderer;
     private readonly scene:Scene;
-    private cube: Mesh;
-    private cp:Vector3;
-    private stats:Stats;
+    private object1: Mesh;
+    private object2: Mesh;
+    private object3: Mesh;
+    private controls:OrbitControls;
+    private stats: Stats;
+    private debug: HTMLDivElement;
     
 
     private constructor(){
         this.camera=new PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1, 1000);
-        this.cp=new Vector3(0,0,2);
-        this.camera.position.setZ(this.cp.z);
+        this.camera.position.setY(4);
+        this.camera.position.setY(4);
+        this.camera.position.setZ(4);
         this.scene=new Scene();
-        this.scene.add(new AxesHelper(5))
         this.renderer=new WebGLRenderer();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
-        new OrbitControls(this.camera, this.renderer.domElement);
-        const geometry=new BoxGeometry();
-        const material=new MeshBasicMaterial( { color: 0x00ff00, wireframe:true })
-        this.cube=new Mesh(geometry, material)
-        this.scene.add(this.cube);
+        this.controls=new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.target.set(8,0,0);
+        const light=new PointLight(0xffffff, 400);
+        light.position.set(10, 10, 10)
+        this.scene.add(light)
+        this.object1=new Mesh(new SphereGeometry(), new MeshPhongMaterial({color: 0xff0000}));
+        this.object1.position.set(4,0,0);
+        this.scene.add(this.object1);
+        this.object1.add (new AxesHelper(5));
+        this.object2=new Mesh(new SphereGeometry(), new MeshPhongMaterial({color: 0x00ff00}));
+        this.object2.position.set(4,0,0);
+        this.object1.add(this.object2);
+        this.object2.add (new AxesHelper(5));
+        this.object3=new Mesh(new SphereGeometry(), new MeshPhongMaterial({color: 0x0000ff}));
+        this.object3.position.set(4,0,0);
+        this.object2.add(this.object3);
+        this.object3.add (new AxesHelper(5));
         window.addEventListener('resize', this.onWindowResize, false);
-        this.stats= new Stats();
-        document.body.appendChild(this.stats.dom);
-
         const gui=new GUI();
-        const cubeFolder=gui.addFolder('Cube');
-        const cubeRotationFolder = cubeFolder.addFolder('Rotation');
-        cubeRotationFolder.add(this.cube.rotation, 'x', 0, Math.PI*2.0);
-        cubeRotationFolder.add(this.cube.rotation, 'y', 0, Math.PI*2);
-        cubeRotationFolder.add(this.cube.rotation, 'z', 0, Math.PI*2);
-        cubeFolder.open();
-        cubeRotationFolder.open();
-        const cubePositionFolder=cubeFolder.addFolder('Position');
-        cubePositionFolder.add(this.cube.position, 'x', -10, 10, 2);
-        cubePositionFolder.add(this.cube.position, 'y', -10, 10, 2);
-        cubePositionFolder.add(this.cube.position, 'z', -10, 10, 2);
-        cubeFolder.open();
-        cubePositionFolder.open();
-        const cubeScaleFolder=cubeFolder.addFolder('Scale');
-        cubeScaleFolder.add(this.cube.scale,'x', -5, 5);
-        cubeScaleFolder.add(this.cube.scale,'y', -5, 5);
-        cubeScaleFolder.add(this.cube.scale,'z', -5, 5);
-        cubeFolder.add(this.cube, 'visible');
-        cubeFolder.open();
+        const object1Folder=gui.addFolder('Object1');
+        object1Folder.add(this.object1.position, 'x', 0, 10, 0.01).name('X-Position');
+        object1Folder.add(this.object1.rotation, 'x', 0, Math.PI*2, 0.01).name('X-Rotation');
+        object1Folder.add(this.object1.scale, 'x', 0, 2, 0.01).name('X-Scale');
+        object1Folder.open();
+        const object2Folder=gui.addFolder('Object2');
+        object2Folder.add(this.object2.position, 'x', 0, 10, 0.01).name('X-Position');
+        object2Folder.add(this.object2.rotation, 'x', 0, Math.PI*2, 0.01).name('X-Rotation');
+        object2Folder.add(this.object2.scale, 'x', 0, 2, 0.01).name('X-Scale');
+        object2Folder.open();
+        const object3Folder=gui.addFolder('Object3');
+        object3Folder.add(this.object3.position, 'x', 0, 10, 0.01).name('X-Position');
+        object3Folder.add(this.object3.rotation, 'x', 0, Math.PI*2, 0.01).name('X-Rotation');
+        object3Folder.add(this.object3.scale, 'x', 0, 2, 0.01).name('X-Scale');
+        object3Folder.open();
+        this.stats=new Stats();
+        document.body.appendChild(this.stats.dom);
+        this.debug=document.getElementById('debug1') as HTMLDivElement
     }
 
     public static getInstance(){
@@ -66,9 +77,36 @@ export class XScene{
     }
     animate=()=>{
         requestAnimationFrame(this.animate);
-        this.cube.rotation.x +=0.01;
-        this.cube.rotation.y +=0.01;
+        this.controls.update();
         this.render();
+        const object1WorldPosition=new Vector3();
+        this.object1.getWorldPosition(object1WorldPosition);
+        const object2WorldPosition=new Vector3();
+        this.object2.getWorldPosition(object2WorldPosition);
+        const object3WorldPosition=new Vector3();
+        this.object3.getWorldPosition(object3WorldPosition);
+        this.debug.innerText=
+        'Red\n'+
+        'Local Pos X : '+
+        this.object1.position.x.toFixed(2)+
+        '\n'+
+        'World Pos X : '+
+        object1WorldPosition.x.toFixed(2)+
+        '\n'+
+        '\nGreen\n'+
+        'Local Pos X : '+
+        this.object2.position.x.toFixed(2)+
+        '\n'+
+        'World Pos X : '+
+        object2WorldPosition.x.toFixed(2)+
+        '\n'+
+        '\nBlue\n'+
+        'Local Pos X : '+
+        this.object3.position.x.toFixed(2)+
+        '\n'+
+        'World Pos X : '+
+        object3WorldPosition.x.toFixed(2)+
+        '\n';
         this.stats.update();
     }
     render(){
